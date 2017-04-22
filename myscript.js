aefc23afa4ead8b6513d1b285413d0ce74eb1d3a
Shannon Byland <shannonbyland@gmail.com>
2017-04-22 10:10:57 -0600

    copied over from codepen

---
+++ myscript.js
@@ -0,0 +1,29 @@
+var randomAnswer;
+var randomNumber;
+
+function getAnswer() {
+  var answers = [
+    "Not today",
+    "As you wish",
+    "Do it, do it right now",
+    "Ask again later",
+    "Unlikely",
+    "Nope",
+    "Yes",
+    "Without a doubt",
+    "YOLO",
+    "I've got a headache",
+    "Don't be stupid",
+    "Have at it"
+  ];
+  randomNumber = Math.floor(Math.random()*answers.length);
+ }
+var askButton = document.getElementById("generate");
+
+askButton.addEventListener("click", function() {
+  getAnswer();
+});
+
+//Clear out the input after the answer
+
+//need to display history of answers
